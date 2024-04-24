9a7a761cc63fa261cc6f5ca3ca40fe4a0880381e
kcyln <2444572529@qq.com>
2024-04-24 19:57:00 +0800

    整数与示例

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	sum := Add(2, 2)
@@ -9,4 +12,13 @@ func TestAdd(t *testing.T) {
 	if sum != expected {
 		t.Errorf("expected '%d' but got '%d'", expected, sum)
 	}
+}
+
+// 示例
+// 测试输出，用 go test -v 运行，会输出测试用例名称
+// 请注意，如果删除注释 「//Output: 6」，示例函数将不会执行。虽然函数会被编译，但是它不会执行。
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// Output: 6
 }
